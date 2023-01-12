0f2f837171e4e24d444b0ece8528bbfc10aea589
L <leonardo.alvirubro@hotmail.com>
2023-01-11 22:42:54 -0300

    feat: Socket and route done

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,14 +1,18 @@
 package com.example.plugins
 
+import com.example.models.TicTacToeGame
+import com.example.socket
 import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(game: TicTacToeGame) {
 
     routing {
+
+        socket(game)
 
     }
 }
